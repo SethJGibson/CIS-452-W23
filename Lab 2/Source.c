85cbf5eb609852321b92c7e152c9e8314c52fda2
SethJGibson <72580436+SethJGibson@users.noreply.github.com>
2023-01-24 21:26:42 -0500

    2nd to Last Update
    - Prompts user for input
    - Receives and parses input
    - Spawns child to execute command
    - Repeats until user enters "quit"
    
    TODO: Collect resource statistic usage

--- Lab 2/Source.c
+++ Lab 2/Source.c
@@ -4,36 +4,51 @@
 #include <stdlib.h>
 #include <string.h>
 
+static const unsigned int BUFFER_SIZE = 64;
+
 int main(int argc, char* argv[]) {
+    // Gonna try something from https://stackoverflow.com/questions/37273193/tokenize-user-input-for-execvp-in-c
 
-    char* input = malloc(sizeof(char) * 64);
-    pid_t childProcessID;
+    char input[64];
+    int status;
 
-    printf("Input: ");
-    fgets(input, sizeof(input), stdin);
-    printf("your input: %s\n", input);
-    //TODO quit
+    printf("You have shell!\n");
 
-    char* command = malloc(sizeof(char) * 64);
-    char* args = malloc(sizeof(char) * 64);
-    command = strsep(&input, " ");
-    args = strsep(&input, "\n");
-    printf("Command: %s  Args: \"%s\"\n", command, args);
+    while (1) {
+        printf("\nuser@gvsu> ");
 
-    if ((childProcessID = fork()) < 0) {
-        printf("Fork Failed");
-        exit(1);
-    }
-    if (childProcessID == 0) {
-        if (execlp(command, command, args, (char*)0) < 0) {
-            perror("exec failed");
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = 0;    // Didn't copy this from above source, went out and double-checked it: https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
+
+        if (strncmp(input, "quit", 4) == 0) {
             exit(1);
         }
-    }
-    else {
-        wait(-1);
-        printf("Parent done");
-        exit(0);
-        //resource usage stats
+
+        char* token = strtok(input, " ");
+        char* args[64];
+        int argCounter = 0;
+
+        while (token != NULL) {
+            args[argCounter] = token;
+            argCounter++;
+            token = strtok(NULL, " ");
+        }
+        args[argCounter] = NULL;
+
+        pid_t childProcessID = fork();
+
+        if (childProcessID < 0) {   // Error
+            printf("Fork process failed, program exit.\n");
+            exit(1);
+        }
+
+        if (childProcessID == 0) {  // Child
+            execvp(args[0], args);
+        }
+        else {
+            wait(&status);
+        }
     }
 }
+
+
