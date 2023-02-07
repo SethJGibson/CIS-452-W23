ff0cc1babc32025ccfd05b872f2623c40bcfae49
SethJGibson <72580436+SethJGibson@users.noreply.github.com>
2023-02-07 13:55:58 -0500

    Eliminated the pipe between last child and parent.
    Parent reading from pipe functionality was failing for unknown reason. Later implementation of Proof.c will phase out need for parent to read from pipe.

--- Project 1/Proof.c
+++ Project 1/Proof.c
@@ -6,11 +6,13 @@
 #include <stdlib.h>
 #include <signal.h>
 
-// Using method from https://code-vault.net/course/46qpfr4tkz:1603732431896/lesson/mrvonl5pmz:1603732432986
+#define READ 0
+#define WRITE 1
 
-int proof(void) {
-	int userInputChildren = 5;
-	int pid, currentChild;
+int main()
+{
+    int userInputChildren = 5;
+    int pid, currentChild;
 
     int fd[userInputChildren + 1][2];
     for (int i = 0; i < userInputChildren; i++) {
@@ -34,6 +36,7 @@ int proof(void) {
     if (pid == 0) { // CHILD
         // Close all not-in-use pipe openings
         for (int i = 0; i < userInputChildren + 1; i++) {
+        //for (int i = 0; i < userInputChildren; i++) {
             for (int j = 0; j < 2; j++) {
                 if (i - currentChild != j) {
                     close(fd[i][j]);
@@ -47,24 +50,29 @@ int proof(void) {
             printf("Error in Child #%d read. Exiting...\n", currentChild);
             exit(1);
         }
+        
         printf("Child #%d received %d!\n", currentChild, hotPotata);
+        
+        if ((currentChild + 1) == userInputChildren)
+        	return 0;
+        
         if (write(fd[currentChild + 1][1], &hotPotata, sizeof(int)) < 0) {
-            printf("Error in Child #%d write. Exiting...\n", currentChild);
+            printf("Error in Child #%d write to [%d][1]. Exiting...\n", currentChild, currentChild + 1);
             exit(1);
         }
 
         close(fd[currentChild][0]);
         close(fd[currentChild + 1][1]);
 
-        return;
+        return 0;
     }
 
     // PARENT
     for (int i = 0; i < userInputChildren + 1; i++) {
         for (int j = 0; j < 2; j++) {
             if ((i == userInputChildren && j == 0) || (i == 0 && j == 1));
-            else
-                close(fd[i][j]);
+            else;
+                //close(fd[i][j]);
         }
     }
     
@@ -74,16 +82,19 @@ int proof(void) {
         printf("Error in Parent write. Exiting...\n");
         exit(1);
     }
-    if (read(fd[userInputChildren][0], &hotPotata, sizeof(int)) < 0) {
-        printf("Error in Parent read. Exiting...\n");
-        exit(1);
-    }
-    printf("Result is %d\n", hotPotata);
+    
+    printf("Parent sent %d!", hotPotata);
+    
+    //if (read(fd[userInputChildren][0], &hotPotata, sizeof(int)) < 0) {
+    //    printf("Error in Parent read. Exiting...\n");
+    //    exit(1);
+    //}
+    //printf("Result is %d\n", hotPotata);
     close(fd[0][1]);
     close(fd[userInputChildren][0]);
 
     //waitpid(pid1, NULL, 0);   // Need to store the pid of each child
     //waitpid(pid2, NULL, 0);   // in order to do clean shutdown here
 
-	return 0;
+    return 0;
 }
