707cff2032460f661dc9c2b48dc13208ff09765d
SethJGibson <72580436+SethJGibson@users.noreply.github.com>
2023-02-14 13:07:27 -0500

    seth's reader.c

--- Lab 5/reader.c
+++ Lab 5/reader.c
@@ -1,49 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <string.h>
 
-struct package
-{
-    int flag;
-    char input[64];
+int shutDownFlag = 0;
+
+struct package {
+	int flag;
+	char input[64];
 };
 
-void main()
+void sigIntHandler(int sigNum) {
+	shutDownFlag = 1;
+}
+
+int main()
 {
+	signal(SIGINT, sigIntHandler);
 
-    int sharedMemoryID;
-    struct myStruct *sharedMemoryPointer;
+	int shmId;
+	key_t key;
+	struct package* sharedMemoryPtr;
+	sharedMemoryPtr->flag = 0;
 
-    key_t sharedKey = ftok("/producer.c", 1);
+	key = ftok("./writer.c", 2);
+	if (key < 0) {
+		perror("Unable to generate producer.c key\n");
+		exit(1);
+	}
 
-    if ((sharedMemoryID = shmget(sharedKey, sizeof(struct myStruct), S_IWUSR | S_IRUSR | S_IRGRP | S_IWGRP | IPC_CREAT)) < 0)
-    {
-        perror("Unable to get shared memory\n");
-        exit(1);
-    }
+	if ((shmId = shmget(key, sizeof(struct package), S_IWUSR | S_IRUSR | S_IRGRP | S_IWGRP | IPC_CREAT)) < 0) {
+		perror("Unable to get shared memory\n");
+		exit(1);
+	}
 
-    if ((sharedMemoryPointer = shmat(sharedMemoryID, 0, 0)) == (void *)-1)
-    {
-        perror("Unable to attach\n");
-        exit(1);
-    }
+	if ((sharedMemoryPtr = shmat(shmId, 0, 0)) == (void*)-1) {
+		perror("Unable to attach\n");
+		exit(1);
+	}
 
-    printf("Int: %d\n", sharedMemoryPointer->myInt);
-    printf("String: %s\n", sharedMemoryPointer->myString);
+	while (1) {
+		while (sharedMemoryPtr->flag == 0);
+		printf("Input from Shared Memory: %s\n", sharedMemoryPtr->input);
 
-    if(shmdt(sharedMemoryPointer) < 0)
-    {
-        perror("Unable to detach\n");
-        exit(1);
-    }
+		memset(sharedMemoryPtr->input, 0, sizeof(sharedMemoryPtr->input));
+		sharedMemoryPtr->flag = 0;
 
-    if (shmctl(sharedMemoryID, IPC_RMID, 0) < 0)
-    {
-        perror("Unable to deallocate\n");
-        exit(1);
-    }
+		if (shutDownFlag) {
+			if (shmdt(sharedMemoryPtr) < 0) {
+				perror("Unable to detach\n");
+				exit(1);
+			}
+		}
+	}
+
+	return 0;
 }
