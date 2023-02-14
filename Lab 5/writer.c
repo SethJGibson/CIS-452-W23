959ea6a2406f612a43b78c1aaa976f1422a2d633
SethJGibson <72580436+SethJGibson@users.noreply.github.com>
2023-02-14 13:27:56 -0500

    Final version

--- Lab 5/writer.c
+++ Lab 5/writer.c
@@ -10,14 +10,14 @@
 int SHUTDOWNFLAG = 0;
 
 void sigHandlerWriter(int sigNum) {
-	switch (sigNum) {
-	case SIGINT:
+    switch (sigNum) {
+    case SIGINT:
         SHUTDOWNFLAG = 1;
-		exit(0);
-	default:
-		printf("Error in SIGINT handling. Exiting...\n");
-		exit(1);
-	}
+        exit(0);
+    default:
+        printf("Error in SIGINT handling. Exiting...\n");
+        exit(1);
+    }
 }
 
 struct package
@@ -26,13 +26,11 @@ struct package
     char input[64];
 };
 
-void
-main()
+void main()
 {
     signal(SIGINT, sigHandlerWriter);
     int sharedMemoryID;
-    struct package *sharedMemoryPointer;
-    printf("hi\n");
+    struct package* sharedMemoryPointer;
     key_t sharedKey = ftok("./writer.c", 1);
 
     if ((sharedMemoryID = shmget(sharedKey, sizeof(struct package), S_IWUSR | S_IRUSR | S_IRGRP | S_IWGRP | IPC_CREAT)) < 0)
@@ -41,36 +39,38 @@ main()
         exit(1);
     }
 
-    if ((sharedMemoryPointer = shmat(sharedMemoryID, 0, 0)) == (void *)-1)
+    if ((sharedMemoryPointer = shmat(sharedMemoryID, 0, 0)) == (void*)-1)
     {
         perror("Unable to attach\n");
         exit(1);
     }
+
     sharedMemoryPointer->flag = 0;
-    while(1){
 
-    char temp[64];
-    printf("Message [Max 64 Charachters]: ");
-    fgets(temp, sizeof(temp), stdin);
-    strcpy(sharedMemoryPointer->input, temp);
+    while (1) {
 
-    sharedMemoryPointer->flag = 1;
-    while(sharedMemoryPointer->flag);
+        char temp[64];
+        printf("Message [Max 64 Charachters]: ");
+        fgets(temp, sizeof(temp), stdin);
+        strcpy(sharedMemoryPointer->input, temp);
 
-    if(SHUTDOWNFLAG){
-        if (shmdt(sharedMemoryPointer) < 0)
-        {
-            perror("Unable to detach\n");
-            exit(1);
+        sharedMemoryPointer->flag = 1;
+        while (sharedMemoryPointer->flag);
+
+        if (SHUTDOWNFLAG) {
+            if (shmdt(sharedMemoryPointer) < 0)
+            {
+                perror("Unable to detach\n");
+                exit(1);
+            }
+            if (shmctl(sharedMemoryID, IPC_RMID, 0) < 0)
+            {
+                perror("Unable to deallocate\n");
+                exit(1);
+            }
+            exit(0);
         }
-        if (shmctl(sharedMemoryID, IPC_RMID, 0) < 0)
-        {
-            perror("Unable to deallocate\n");
-            exit(1);
-        }
-        exit(0);
+
     }
 
-    }
-    
 }
