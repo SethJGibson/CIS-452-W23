b8e56217e5b43d8ffcacca88c20d98f1999d1589
David Geisel <geiseld@mail.gvsu.edu>
2023-02-14 12:58:59 -0500

    writer done probably

--- Lab 5/writer.c
+++ Lab 5/writer.c
@@ -5,24 +5,35 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <string.h>
+#include <signal.h>
 
 int SHUTDOWNFLAG = 0;
 
+void sigHandlerWriter(int sigNum) {
+	switch (sigNum) {
+	case SIGINT:
+        SHUTDOWNFLAG = 1;
+		exit(0);
+	default:
+		printf("Error in SIGINT handling. Exiting...\n");
+		exit(1);
+	}
+}
+
 struct package
 {
-    int flag = 0;
+    int flag;
     char input[64];
 };
 
 void
 main()
 {
-
+    signal(SIGINT, sigHandlerWriter);
     int sharedMemoryID;
     struct package *sharedMemoryPointer;
-    key_t sharedKey = ftok("/producer.c", 1);
-
-    while(1){
+    sharedMemoryPointer->flag = 0;
+    key_t sharedKey = ftok("./writer.c", 1);
 
     if ((sharedMemoryID = shmget(sharedKey, sizeof(struct package), S_IWUSR | S_IRUSR | S_IRGRP | S_IWGRP | IPC_CREAT)) < 0)
     {
@@ -36,13 +47,30 @@ main()
         exit(1);
     }
 
-    
-    strcpy(sharedMemoryPointer->myString, temp);
+    while(1){
 
-    if (shmdt(sharedMemoryPointer) < 0)
-    {
-        perror("Unable to detach\n");
-        exit(1);
+    char temp[64];
+    printf("Message [Max 64 Charachters]: ");
+    fgets(temp, sizeof(temp), stdin);
+    strcpy(sharedMemoryPointer->input, temp);
+
+    sharedMemoryPointer->flag = 1;
+    while(sharedMemoryPointer->flag);
+
+    if(SHUTDOWNFLAG){
+        if (shmdt(sharedMemoryPointer) < 0)
+        {
+            perror("Unable to detach\n");
+            exit(1);
+        }
+        if (shmctl(sharedMemoryID, IPC_RMID, 0) < 0)
+        {
+            perror("Unable to deallocate\n");
+            exit(1);
+        }
+        exit(0);
     }
+
     }
+    
 }
